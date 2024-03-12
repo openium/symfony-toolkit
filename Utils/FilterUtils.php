064876473e86608db3bc1bf19c86619df151bfe9
Thomas LEDUC <thomaslmoi15@hotmail.fr>
2024-03-12 10:40:25 +0100

    Add constants for filter parameters in FilterUtils
    The commit introduces constants for 'search', 'page', 'limit', 'order', and 'order-by' filter parameters in the FilterUtils class. This change enhances code readability by replacing magic strings and allows for easier future modifications for filter parameters.

--- Utils/FilterUtils.php
+++ Utils/FilterUtils.php
@@ -6,6 +6,12 @@ use Symfony\Component\HttpFoundation\Request;
 
 class FilterUtils
 {
+    public const SEARCH_PARAMETER = 'search';
+    public const PAGE_PARAMETER = 'page';
+    public const LIMIT_PARAMETER = 'limit';
+    public const ORDER_PARAMETER = 'order';
+    public const ORDER_BY_PARAMETER = 'order-by';
+
     /**
      * generateFromRequest
      *
@@ -15,11 +21,21 @@ class FilterUtils
      */
     public static function generateFromRequest(Request $request): FilterParameters
     {
-        $search = $request->query->has('search') ? $request->query->getString('search') : null;
-        $page = $request->query->has('page') ? $request->query->getInt('page') : null;
-        $limit = $request->query->has('limit') ? $request->query->getInt('limit') : null;
-        $order = $request->query->has('order') ? $request->query->getString('order') : null;
-        $orderBy = $request->query->has('order-by') ? $request->query->getString('order-by') : null;
+        $search = $request->query->has(self::SEARCH_PARAMETER)
+            ? $request->query->getString(self::SEARCH_PARAMETER)
+            : null;
+        $page = $request->query->has(self::PAGE_PARAMETER)
+            ? $request->query->getInt(self::PAGE_PARAMETER)
+            : null;
+        $limit = $request->query->has(self::LIMIT_PARAMETER)
+            ? $request->query->getInt(self::LIMIT_PARAMETER)
+            : null;
+        $order = $request->query->has(self::ORDER_PARAMETER)
+            ? $request->query->getString(self::ORDER_PARAMETER)
+            : null;
+        $orderBy = $request->query->has(self::ORDER_BY_PARAMETER)
+            ? $request->query->getString(self::ORDER_BY_PARAMETER)
+            : null;
         return new FilterParameters(
             $search,
             $page,
